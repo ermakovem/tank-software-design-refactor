6344be9487d2e0ea648286bfb04fef027689a117
ermakovem <144175328+ermakovem@users.noreply.github.com>
2023-09-24 19:07:42 +0300

    FIXED TWITCHES

--- src/main/java/ru/mipt/bit/platformer/Tank.java
+++ src/main/java/ru/mipt/bit/platformer/Tank.java
@@ -9,7 +9,7 @@ import static com.badlogic.gdx.math.MathUtils.*;
 import static ru.mipt.bit.platformer.util.GdxGameUtils.continueProgress;
 
 public class Tank implements GameObject{
-    private final GridPoint2 coordinates;
+    private GridPoint2 coordinates;
     private final GridPoint2 destinationCoordinates;
     private float movementProgress = 1f;
     private final float movementSpeed;
@@ -17,7 +17,7 @@ public class Tank implements GameObject{
 
     public Tank(GridPoint2 coordinates, float movementSpeed) {
         this.coordinates = coordinates;
-        this.destinationCoordinates = coordinates;
+        this.destinationCoordinates = new GridPoint2(coordinates);
         this.movementSpeed = movementSpeed;
     }
 
@@ -42,7 +42,7 @@ public class Tank implements GameObject{
     public void updateState(float deltaTime) {
         movementProgress = continueProgress(movementProgress, deltaTime, movementSpeed);//continue movement progress
         if (isEqual(movementProgress, 1f)) { // if movement has ended set coordinates
-            //coordinates.set(destinationCoordinates);
+            coordinates.set(destinationCoordinates);
         }
     }
 
